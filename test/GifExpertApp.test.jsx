3b986420370ee37506f021ccc6eb16216c48c7b1
FranLobo <floboflores@gmail.com>
2023-02-27 21:51:53 +0100

    Add two tests in  GifExpertApp.test '

--- test/GifExpertApp.test.jsx
+++ test/GifExpertApp.test.jsx
@@ -1,3 +1,34 @@
+import { fireEvent, getByPlaceholderText, render, screen } from "@testing-library/react";
+import { GifExpertApp } from "../src/GifExpertApp";
+
 describe("Testing in <GifExpertApp>", () => {
-  test("Should ", () => {});
+  test("Should be add a new category that don´t exist ", () => {
+
+    render(<GifExpertApp/>);
+
+    const input = screen.getByRole('textbox');
+    const form = screen.getByRole('form');
+
+    fireEvent.input(input,{target: {value: 'Wolf'}});
+    fireEvent.submit(form);
+
+    expect(screen.getByText('Wolf')).toBeTruthy();
+  
+    expect(screen.getByPlaceholderText("Search Gifs...")).toBeTruthy();
+
+  });
+
+  test("Should be not  add a new category that exist ", () => {
+
+    render(<GifExpertApp/>);
+
+    const input = screen.getByRole('textbox');
+    const form = screen.getByRole('form');
+
+    fireEvent.input(input,{target: {value: 'Wolf'}});
+    fireEvent.submit(form);
+  
+    expect(screen.getAllByText('Wolf').length).toBe(1);
+
+  });
 });
