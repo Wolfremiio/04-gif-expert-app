41679d34a8ecee1f665a3bda2975e77fadff295b
FranLobo <floboflores@gmail.com>
2023-02-27 20:05:39 +0100

    New  test of GifGrid with Mock '

--- test/components/GifGrid.test.jsx
+++ test/components/GifGrid.test.jsx
@@ -1,7 +1,8 @@
 import { render, screen } from "@testing-library/react";
 import { GifGrid } from "../../src/components/GifGrid";
+import { useFetchGifs } from "../../src/hooks/useFetchGifs";
 
-
+jest.mock("../../src/hooks/useFetchGifs");
 
 
 describe('Testing in <GigGrid>', () => {
@@ -10,10 +11,43 @@ describe('Testing in <GigGrid>', () => {
 
     test('Should be show the loading ', () => {
         
+        useFetchGifs.mockReturnValue({
+            images:[],
+            isLoading: true,
+        });
+        
         render(<GifGrid category = { category }/>)
 
         expect(screen.getByText('Loading...'));
         expect(screen.getByText(category));
      
+    });
+
+    test('Should be show items when loading  the useFetchGif´s images  ', () => {
+        const gifs= [{
+            id: 'ABC',
+            title: 'Wolf',
+            url: 'https://localhost/Wolf.jpg',
+        },
+
+        {
+            id: 'CBA',
+            title: 'Snork',
+            url: 'https://localhost/Snork.jpg',
+        }]
+
+
+        useFetchGifs.mockReturnValue({
+            images: gifs,
+            isLoading: false,
+        });
+        render(<GifGrid category = { category }/>)
+        
+        expect(screen.getAllByRole('img').length).toBe(2)
+        //screen.debug();
+
+
+
+
     });
 })
